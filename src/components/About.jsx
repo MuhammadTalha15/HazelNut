af0e94af7f17c65e38bbb13cd3e24445fe186d5c
MuhammadTalha15 <talhaz5518@gmail.com>
2024-07-24 21:56:52 +0500

    About Page Extension

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
                 <div className="txt cont">
                     <div className="abt-txt-cont-cnt">
                         <h1 className="abt-head">ABOUT</h1>
-                        <p className="abt-descrip">With a foundation built on both Intermediate and Beginner-level creations, I am on a journey to evolve into a seasoned Computer Scientist.</p>
+                        <p className="abt-descrip">I am Muhammad Talha, an Intermediate Computer Science student currently studying in college. With a passion for web development and computer science, I am dedicated to enhancing my skills and knowledge in the field.</p>
                         <p className="abt-descrip-main">As an intermediate-level computer science student, I am deeply immersed in various technology projects, showcasing a versatile skill set in web development, desktop applications, and data science. My primary tools include JavaScript, Python, React, Express, MongoDB, and C++. Javascript, React are done for my Frontend Projects, comprising Express and MongoDB responsible for Backend. Python is done for Data Science and Desktop Application with company of C++.</p>
                     </div>
                 </div>
@@ -45,11 +45,40 @@ const About = () => {
                     </div>
                 </div>
             </section>
-            {/* <section className="abt-scnd-sec">
-                <div className="ext-lft"></div>
+            <section className="abt-scnd-sec">
+                <div className="ext-lft">
+                    <div className="tch-sklls-cnt">
+                        <h2 className="skls-head">Technical Skills</h2>
+                        <ol className="sklls-ol">
+                            <li className='sklls-li'>Problem Solving</li>
+                            <li className='sklls-li'>Data Structuring</li>
+                            <li className='sklls-li'>Communication</li>
+                            <li className='sklls-li'>Time Managment</li>
+                            <li className='sklls-li'>Collaborations</li>
+                            <li className='sklls-li'>Adaptability</li>
+                        </ol>
+                    </div>
+                    <div className="prfl-sklls-cnt">
+                        <h2 className="skls-head">Professional Skills</h2>
+                        <ol className="sklls-ol">
+                            <li className='sklls-li'>Website Development</li>
+                            <li className='sklls-li'>Web App Development</li>
+                            <li className='sklls-li'>Program Scripting</li>
+                            <li className='sklls-li'>Database Management</li>
+                            <li className='sklls-li'>Website Designing</li>
+                            <li className='sklls-li'>Cloud Computing</li>
+                        </ol>
+                    </div>
+                    <div className="prsnl-sklls-cnt">
+                        <h2 className="skls-head">Personal Skills</h2>
+                        <ol className="sklls-ol per">
+                            <li className='sklls-li'>Fitness Training, Creativity, Team Work</li>
+                        </ol>
+                    </div>
+                </div>
                 <div className="mid"></div>
                 <div className="ext-rgt"></div>
-            </section> */}
+            </section>
         </>
     )
 }
