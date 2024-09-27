ccfc1e337a66dc75cf49c96038627f56cadf931b
MuhammadTalha15 <talhaz5518@gmail.com>
2024-09-27 10:54:17 +0500

    Design Updated Color Scheme Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,16 +13,20 @@ const About = () => {
         <>
             <Loader />
             <section className="abt-hero-sec">
-                <div className="txt cont">
-                    <div className="abt-txt-cont-cnt">
-                        <h1 className="abt-head">ABOUT</h1>
-                        <p className="abt-descrip">I am Muhammad Talha, an Intermediate Computer Science student currently studying in college. With a passion for web development and computer science, I am dedicated to enhancing my skills and knowledge in the field.</p>
-                        <p className="abt-descrip-main">As an intermediate-level computer science student, I am deeply immersed in various technology projects, showcasing a versatile skill set in web development, desktop applications, and data science. My primary tools include JavaScript, Python, React, Express, MongoDB, and C++. Javascript, React are done for my Frontend Projects, comprising Express and MongoDB responsible for Backend. Python is done for Data Science and Desktop Application with company of C++.</p>
-                    </div>
+                <div className="abt-sec-head">
+                    <h1 className="abt-mn-hd">ABOUT ME</h1>
                 </div>
-                <div className="rght cont">
-                    <div className="rght-tble-cont-cnt">
-                        <table className='abt-table'>
+                <div className="prsl-dta-sec">
+                    <div className="heading-cnt">
+                        <h2 className="prsl-hd-fir">PERSONAL INFO</h2>
+                        <h1 className="prsl-mn-hd">Learn About Me</h1>
+                    </div>
+                    <div className="abt-txt-cnt">
+                        <p className="abt-prsl-txt">I am Muhammad Talha, an Intermediate Computer Science student currently studying in college. With a passion for Computer Engineering and Computer Sciences, I am dedicated to enhancing my skills and knowledge in the field.</p>
+                        <p className="abt-prsl-txt">As an intermediate-level computer science student, I am deeply immersed in various technology projects, showcasing a versatile skill set in web development, desktop applications, and data science. My primary tools include JavaScript, Python, React, Express, MongoDB, and C++.</p>
+                    </div>
+                    <div className="tble-cnt">
+                        <table>
                             <tr>
                                 <th>Name:</th>
                                 <td>Muhammad Talha</td>
@@ -35,77 +39,27 @@ const About = () => {
                                 <th>Email:</th>
                                 <td>talhaz5518@gmail.com</td>
                             </tr>
+                        </table>
+
+                        <table>
                             <tr>
                                 <th>Adress:</th>
                                 <td>Naval Anchorage, Islamabad</td>
                             </tr>
                             <tr>
                                 <th>Phone:</th>
-                                <td>{`(+92)333-545-9231`}</td>
+                                <td>(+92)334-534-4658</td>
                             </tr>
                             <tr>
                                 <th>Language:</th>
-                                <td>English, Urdu</td>
+                                <td>Urdu, English</td>
                             </tr>
                         </table>
                     </div>
                 </div>
             </section>
             <section className="abt-scnd-sec">
-                <div className="ext-lft">
-                    <div className="tch-sklls-cnt">
-                        <h2 className="skls-head">Technical Skills</h2>
-                        <ol className="sklls-ol">
-                            <li className='sklls-li'>Problem Solving</li>
-                            <li className='sklls-li'>Data Structuring</li>
-                            <li className='sklls-li'>Communication</li>
-                            <li className='sklls-li'>Time Managment</li>
-                            <li className='sklls-li'>Collaborations</li>
-                            <li className='sklls-li'>Adaptability</li>
-                        </ol>
-                    </div>
-                    <div className="prfl-sklls-cnt">
-                        <h2 className="skls-head">Professional Skills</h2>
-                        <ol className="sklls-ol">
-                            <li className='sklls-li'>Website Development</li>
-                            <li className='sklls-li'>Web App Development</li>
-                            <li className='sklls-li'>Program Scripting</li>
-                            <li className='sklls-li'>Database Management</li>
-                            <li className='sklls-li'>Website Designing</li>
-                            <li className='sklls-li'>Cloud Computing</li>
-                        </ol>
-                    </div>
-                    <div className="prsnl-sklls-cnt">
-                        <h2 className="skls-head">Personal Skills</h2>
-                        <ol className="sklls-ol per">
-                            <li className='sklls-li'>Fitness Training, Creativity, Team Work</li>
-                        </ol>
-                    </div>
-                </div>
-                <div className="mid"></div>
-                <div className="ext-rgt">
-                    <div className="wtdo-cnt">
-                        <h2 className="skls-head">What can I do?</h2>
-                        <p className="wtdo-p">I have experience in developing and designing websites and web applications. Additionally, I am skilled in writing scripts for various general purposes and creating full stack projects.</p>
-                    </div>
-                    <div className="sftwre-cnt">
-                        <h2 className="skls-head">Software I use</h2>
-                        <div className="sftwr-logo-cnt">
-                            <img title='Vs Code' className='sftwre-logo' src={vscode} alt="" />
-                            <img title='Figma' className='sftwre-logo' src={figma} alt="" />
-                            <img title='Unsplash' className='sftwre-logo' src={unsplash} alt="" />
-                            <img title='Adobe Photoshop' className='sftwre-logo' src={photoshop} alt="" />
-                        </div>
-                    </div>
-                    <div className="hby-cnt">
-                        <h2 className="skls-head">Hobbies & Interests</h2>
-                        <ol className="hby-ol">
-                            <li className="hby-li">Content Writing</li>
-                            <li className="hby-li">Coding</li>
-                            <li className="hby-li">Learning New Technologies</li>
-                        </ol>
-                    </div>
-                </div>
+
             </section>
         </>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
 import '../styles/footer.css'
-import Logo from '../assets/Logo/logo.png'
+import Logo from '../assets/Logo/footer-Logo.png'
 import Facebook from '../assets/Icons/facebook.svg'
 import Instagram from '../assets/Icons/instagram.svg'
 import Github from '../assets/Icons/github.svg'
+import Stack from '../assets/Icons/stack-overflow.svg'
 import { Link, useLocation } from 'react-router-dom'
 import SmoothScrollBtn from './SmoothScrollBtn'
 
@@ -13,7 +14,7 @@ const Footer = () => {
 
   const path = useLocation();
   useEffect(() => {
-    scrollFooterTop();  
+    scrollFooterTop();
   }, [path]);
 
   const scrollFooterTop = () => {
@@ -23,42 +24,32 @@ const Footer = () => {
   return (
     <>
       <section className="footer">
-        <div className="ftr-uppr">
-          <SmoothScrollBtn/>
-          <div className="ftr-cnt">
-            <img src={Logo} alt="" className='ftr-logo' />
-            <p className="ftr-lgo-aftr-txt">Your Vission, My Expertise Feel free to Contact for your Solutions and Products.</p>
-            <form action="" className='sub-form'>
-              <input type="text" name="sub" id="sub" className='sub' placeholder='Email' />
-              <input type="submit" className='sub-btn' value="SUBSCRIBE" />
-            </form>
-            <div className="social-links">
-              <a href="#" className='social-link-a' target='_blank'><img className='ftr-social-link-img' src={Facebook} alt="" /></a>
-              <a href="#" className='social-link-a' target='_blank'><img className='ftr-social-link-img' src={Instagram} alt="" /></a>
-              <a href="#" className='social-link-a' target='_blank'><img className='ftr-social-link-img' src={Github} alt="" /></a>
-            </div>
-          </div>
-          <div className="ftr-cnt mid">
-              <h2 className="ftr-nav">Navigation</h2>
-              <ol className='ftr-ol'>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/About">About</Link></li>
-                <li><Link to="/Projects">Projects</Link></li>
-                <li><Link to="/Contact">Contact</Link></li>
-              </ol>
-          </div>
-          <div className="ftr-cnt mid">
-          <h2 className="ftr-nav">Get in Touch</h2>
-              <ol className='ftr-ol'>
-                <li className='ftr-info'>(+92)333-5281312</li>
-                <li className='ftr-info'>0333-5467312</li>
-                <li className='ftr-info'>talhaz5518@gmail.com</li>
-                <li className='ftr-info'>Contact</li>
-              </ol>
+        <div className="uppr-txt-cnt">
+          <div className="txt-cnt">
+            <p className="ftr-upr-txt">Your Vission, My Expertise Feel free to Contact for your Solutions and Products.</p>
           </div>
         </div>
-        <div className="ftr-lwr">
-          <p className="footer-text">{`Copyright © ${currentYear} Website | Muhammad Talha®. All rights reserved.`}</p>
+        <div className="ftr-lgo-cnt">
+          <img className='ftr-lgo' src={Logo} alt="Footer Logo" />
+        </div>
+        <div className="ftr-nav-cnt">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/About">About</Link></li>
+            <li><Link to="/Projects">Projects</Link></li>
+            <li><Link to="/Contact">Contact</Link></li>
+          </ul>
+        </div>
+        <div className="ftr-scl-lnk-cnt">
+          <ul className='scl'>
+            <li><a href="#"><img className='ftr-scl-img' src={Facebook} alt="" /></a></li>
+            <li><a href="#"><img className='ftr-scl-img' src={Instagram} alt="" /></a></li>
+            <li><a href="#"><img className='ftr-scl-img' src={Github} alt="" /></a></li>
+            <li><a href="#"><img className='ftr-scl-img' src={Stack} alt="" /></a></li>
+          </ul>
+        </div>
+        <div className="cpy-rht-cnt">
+          <p className="cpy-rgt-txt"> Copyright © {currentYear} Website | Muhammad Talha®. All rights reserved. </p>
         </div>
       </section>
     </>
